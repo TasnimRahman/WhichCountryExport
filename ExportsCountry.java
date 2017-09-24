995e7b93620e835db5dab586188265001cc02c53
ASUS <ASUS>
2017-09-24 11:41:07 -0700

    add listExportsTwoProducts method to test which country exports the elements and add tester to test the method

--- ExportsCountry.java
+++ ExportsCountry.java
@@ -28,10 +28,26 @@ public class ExportsCountry
          }
         return result;
      }
+     
+    public void listExportsTwoProducts(CSVParser parser , String exportItem1 , String exportItem2){
+          
+        for (CSVRecord record : parser) {
+            
+            String countryName = record.get("Exports");
+            
+            if (countryName.contains(exportItem1) && countryName.contains(exportItem2) ) {
+                
+                String export = record.get("Country");
+                System.out.println(export);
+                  }
+            }
+    }
        
     public void tester() {
         FileResource fr = new FileResource();
         CSVParser parser = fr.getCSVParser();
         System.out.println(countryInfo(parser, "Germany"));
+        parser = fr.getCSVParser();
+        listExportsTwoProducts(parser ,"gold","diamonds");
     }
 }
