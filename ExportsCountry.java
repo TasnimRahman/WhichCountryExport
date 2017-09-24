af0bd4ec2ad2da97acb0f6c4c1f767a44ebdbc98
ASUS <ASUS>
2017-09-24 11:37:22 -0700

    add  String countryInfo method to find the information about the country and add tester to test the method

--- ExportsCountry.java
+++ ExportsCountry.java
@@ -1,6 +1,37 @@
 
 
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+
 public class ExportsCountry
 {
-    
+    public String countryInfo(CSVParser parser, String country) {
+        String result = "";
+
+        for (CSVRecord record : parser) {
+            
+            String countryName = record.get("Country");
+            
+            if (countryName.contains(country)) {
+                
+                String export = record.get("Exports");
+                System.out.println(export);
+                
+                String value = record.get("Value (dollars)");
+                System.out.println(value);
+                
+                result = country+" :"+export+" :"+value;
+                
+             }
+         }
+        return result;
+     }
+       
+    public void tester() {
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser();
+        System.out.println(countryInfo(parser, "Germany"));
+    }
 }
